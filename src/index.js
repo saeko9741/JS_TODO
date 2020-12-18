8b911eace667e3856d0350ad484669436454dc69
saeko <58896213+saeko9741@users.noreply.github.com>
2020-12-18 15:33:33 +0900

    Add button functions

--- src/index.js
+++ src/index.js
@@ -14,8 +14,26 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 完了ボタンタグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // 完了ボタンにイベント追加
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // 削除ボタンタグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  // 削除ボタンにイベント追加
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの小要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
